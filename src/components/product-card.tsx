6018beb77a528610f2a028512d493768cbc54e36
roberth raimundo <roberth.raimundo308@gmail.com>
2026-02-17 14:39:37 +0000

    agora sobre o card do produto na sessão vitrine de pedidos: diminua em 5

--- src/components/product-card.tsx
+++ src/components/product-card.tsx
@@ -11,18 +11,18 @@ type ProductCardProps = {
 export const ProductCard = ({ product, onSelect }: ProductCardProps) => {
   return (
     <div className="bg-white rounded-xl border border-primary/10 hover:border-primary hover:shadow-2xl transition-all duration-500 flex flex-col text-left h-full select-none group">
-      <div className="p-4 pb-0">
+      <div className="p-2 pb-0">
         <div className="relative w-full aspect-square rounded-lg overflow-hidden">
           <Image 
             src={product.image} 
             alt={product.shortDesc} 
             fill
             sizes="(max-width: 640px) 50vw, 33vw"
-            className="object-cover transition-transform duration-500" 
+            className="object-cover" 
           />
         </div>
       </div>
-      <div className="p-4 pb-6 w-full flex flex-col flex-grow">
+      <div className="px-2 pt-4 pb-6 w-full flex flex-col flex-grow">
         {product.name && <h3 className="font-headline text-xl sm:text-2xl lg:text-3xl text-primary mb-2 leading-tight tracking-wide">{product.name}</h3>}
         <p className="text-primary/60 text-xs font-body mb-6 md:mb-10 leading-relaxed line-clamp-2">{product.shortDesc}</p>
         <div className="mt-auto w-full flex justify-center">
